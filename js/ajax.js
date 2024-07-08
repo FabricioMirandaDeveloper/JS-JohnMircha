8a9941b28a765a9e1cbbb589db19559b26d16e2d
Italiano <mirandaquezadafabriciojoel@gmail.com>
2024-07-07 21:00:00 -0500

    ajax con fetch + async await

---
+++ js/ajax.js
@@ -0,0 +1,82 @@
+(() => {
+    const xhr = new XMLHttpRequest(),
+        $xhr = document.getElementById("ahr"),
+        $fracment = document.createDocumentFragment();
+
+    xhr.addEventListener("readystatechange", (e) => {
+        if (xhr.readyState !== 4) return;
+        /* console.log(xhr); */
+
+        if (xhr.status >= 200 && xhr.status < 300) {
+            /* console.log("exito"); */
+            let json = JSON.parse(xhr.responseText)
+            /* console.log(json); */
+            json.forEach(e => {
+                let $li = document.createElement("li")
+                $li.innerHTML = `${e.name} -- ${e.phone} -- ${e.email}`
+                $fracment.appendChild($li)
+            });
+            $xhr.appendChild($fracment)
+        } else {
+            /* console.log("error"); */
+            let message = xhr.statusText || "Ocurrio un error"
+            $xhr.innerHTML = `Error ${xhr.status}: ${message}`
+        }
+
+    })
+    /*  xhr.open("GET","https://jsonplaceholder.typicode.com/users") */
+    xhr.open("GET", "/assets/users.json")
+    xhr.send()
+})();
+(() => {
+    const $fetch = document.getElementById("fetch"),
+        $fracment = document.createDocumentFragment();
+    fetch("https://jsonplaceholder.typicode.com/users")
+    /* fetch("/assets/users.json") */
+        /* .then((res) => {
+            console.log(res)
+          
+            return res.ok ? res.json() : Promise.reject(res)
+        }) */
+        .then((res) => res.ok ? res.json() : Promise.reject(res))
+        .then(json => {
+            /* console.log(json) */
+            json.forEach(e => {
+                let $li = document.createElement("li")
+                $li.innerHTML = `${e.name} -- ${e.phone} -- ${e.email}`
+                $fracment.appendChild($li)
+            });
+            $fetch.appendChild($fracment)
+        })
+        .catch((err) => {
+            /* console.log("error",err) */
+            let message = err.statusText || "Ocurrio un error"
+            $fetch.innerHTML = `Error ${err.status}: ${message}`
+        })
+        /* .finally(() => console.log("Se ejecutara independientemente de la respuesta")) */
+})(),
+
+(() => {
+    const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+    async function getData() {
+        try {
+            let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+            json = await res.json()
+            /* console.log(res);
+            console.log(json); */
+            if(!res.ok) throw {status: res.status, statusText: res.statusText}
+            json.forEach(e => {
+                let $li = document.createElement("li")
+                $li.innerHTML = `${e.name} -- ${e.phone} -- ${e.email}`
+                $fragment.appendChild($li)
+            });
+            $fetchAsync.appendChild($fragment)
+        } catch (error) {
+            console.log(error);
+            let message = error.statusText || "Ocurrio un error"
+            $fetchAsync.innerHTML = `Error ${error.status}: ${message}`
+        }
+    }
+    getData()
+})()
--- js/ejercicios.js
+++ js/ejercicios.js
@@ -123,13 +123,19 @@ invertirCadena("Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedit
 
 // 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
 
-const palindromo = (cadena) => {
+/* const palindromo = (cadena) => {
   let cadena = cadena.toLowerCase();
   let reverseCadena = cadena.split("").reverse().join("");
   (cadena === reverseCadena) ? console.log("true") : console.log("false");
 }
 
 palindromo("fabricio")
-palindromo("salas")
+palindromo("salas") */
 
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+
+const eliminarPatron = (texto, patron) => {
+  console.log(texto.replace(new RegExp(patron, 'ig'), ''))
+}
+eliminarPatron("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+
